93e5d2ce7194f9c97d11dd39af83fb4ec7796136
ARVIND-46 <114347982+ARVIND-46@users.noreply.github.com>
2025-03-30 16:52:55 +0530

    some starting working

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import '../styles/styles.css'
+import Fbicon from'../img/fb.png'
+import Inicon from'../img/instra.png'
+import Ticon from '../img/twitter.png'
+import Yicon from '../img/yt.png'
 
 const Footer = () => {
   return (
-    <p></p>
+   <div className='footer'>
+    <p>Kpriet.edu.in</p>
+    <img src={Fbicon} alt="" />
+    <img src={Inicon} alt="" />
+    <img src={Ticon} alt="" />
+    <img src={Yicon} alt="" />
+    <p>| KPRIET online</p>
+
+   </div>
   )
 }
 
