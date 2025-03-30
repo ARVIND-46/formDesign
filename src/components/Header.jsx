7298a19342e4aeccbad0ef17b5afe3940d47b53c
ARVIND-46 <114347982+ARVIND-46@users.noreply.github.com>
2025-03-30 10:11:46 +0530

    work have been started

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import '../styles/styles.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+   <div className='header'>
+    <h3>KPR Institute of Engineering and Technology</h3>
+   </div>
   )
 }
 
